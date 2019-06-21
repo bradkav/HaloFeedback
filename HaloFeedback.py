4cc185f0944aaf89d4f8faf971677f0f97a7c905
bradkav <bradley.kavanagh@cantab.net>
2019-06-21 15:10:36 +0200

    Updated default method

--- HaloFeedback.py
+++ HaloFeedback.py
@@ -119,7 +119,7 @@ class DistributionFunction():
     
     
     
-    def dfdt(self, r0, v_orb, v_cut=-1, method = 1, n_kick = 1):
+    def dfdt(self, r0, v_orb, v_cut=-1, method = 2, n_kick = 1):
         """Time derivative of the distribution function f(eps).
         
         Parameters:
